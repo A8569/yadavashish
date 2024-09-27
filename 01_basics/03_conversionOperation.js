aa1d6509d24679d3179654ee6dd99e0effec4749
Ashish Yadav <navy936978@gmail.com>
2024-09-27 10:05:59 +0000

    conversions of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "ashish "
+
+// console.log(typeof score);//
+// console.log(typeof(score));//
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);//
+// console.log(valueInNumber);//
+
+
+// "33" => 33
+// "33abc" =>Nan
+// true => 1; false => 0
+
+let isLoggedIn = "ashish"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log( booleanIsLoggedIn);
+
+// 1=> true; 0=> fasle
+//"" => fasle
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
