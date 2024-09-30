5e4acaeb53b26dabe0eddf0541967b44311e8cba
Ashish Yadav <navy936978@gmail.com>
2024-09-30 13:19:09 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march" 
+
+switch (month) {
+    
+    case "january":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+    default:
+        console.log("daefault case match");
+        
+        break;
+}
